44eb49bed2cd6dddcb2730b60a08b102733a56ee
Soh1121 <satou.shg@gmail.com>
2022-01-02 12:08:41 +0900

    [update]dockerとdocker-composeを導入

--- initialize.sh
+++ initialize.sh
@@ -15,17 +15,20 @@ apt install language-pack-ja -y
 update-locale LANG=ja_JP.UTF8
 
 apt install -y \
+  apt-transport-https \
 	automake \
 	bison \
 	build-essential \
+  ca-certificates \
 	curl \
+  gnupg-agent \
 	libevent-dev \
 	libncurses5-dev \
+  software-properties-common \
 	tree \
 	pkg-config \
 	xz-utils \
 	zsh
-
 
 #---------------------------------------------------#
 # set TMUX                                          #
@@ -66,6 +69,25 @@ git clone https://github.com/anyenv/anyenv $ROOT/.anyenv
 echo '########################################' >> $ROOT/.zshrc
 echo '# anyenv' >> $ROOT/.zshrc
 echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> $ROOT/.zshrc
+
+#---------------------------------------------------#
+# set docker                                        #
+#---------------------------------------------------#
+apt remove docker docker-engine docker.io containerd runc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io
+
+#---------------------------------------------------#
+# set docker-compose                                #
+#---------------------------------------------------#
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 #---------------------------------------------------#
 # add .zshrc                                        #
